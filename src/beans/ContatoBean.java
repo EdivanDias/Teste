9a1b82745e5c6adc2181c4d522fc8775912b6e59
Edivan Dias <musica_ed@yahoo.com.br>
2013-03-19 02:23:53 -0300

    Signed-off-by: Edivan Dias <musica_ed@yahoo.com.br>

--- src/beans/ContatoBean.java
+++ src/beans/ContatoBean.java
@@ -5,25 +5,29 @@ import Model.Contato;
 public class ContatoBean {
 
 	private Contato contato = new Contato();
-	
-	public ContatoBean(){
+
+	public ContatoBean() {
 		System.out.println("Criando bean ContatoBean");
 	}
-	
-	public String gravar(String paginarecebe){
-		System.out.println("Gravando contato "+ contato.getNome());
-		System.out.println("Gravando contato "+ contato.getTelefone());
-		System.out.println("Gravando contato "+ contato.getNascimento());
-		System.out.println("Macaco Banana");
+
+	public String gravar(String paginarecebe) {
+		int i = 5;
+		while (i > 0) {
+			System.out.println("Gravando contato " + contato.getNome());
+			System.out.println("Gravando contato " + contato.getTelefone());
+			System.out.println("Gravando contato " + contato.getNascimento());
+			System.out.println("Macaco Banana");
+			i--;
+		}
 		return paginarecebe;
 	}
-	
-	public void alterar(){
-		
+
+	public void alterar() {
+
 	}
-	
-	public void deletar(){
-		
+
+	public void deletar() {
+
 	}
 
 	public Contato getContato() {
@@ -33,5 +37,5 @@ public class ContatoBean {
 	public void setContato(Contato contato) {
 		this.contato = contato;
 	}
-	
+
 }
